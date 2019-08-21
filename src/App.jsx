38dec374f6ecc5d5a898c48a357aa563fc39000e
johnhaus <john.haus@outlook.com>
2019-08-20 21:38:21 -0300

    changed list display

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 
 class App extends Component {
   render() {
@@ -16,7 +16,7 @@ class App extends Component {
           <div id="page-body">
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/articles-list" component={ArticlesListPage} />
             <Route path="/article/:name" component={ArticlePage} />
           </div>
         </div>
