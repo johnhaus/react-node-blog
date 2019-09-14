e7a997fedf3436de95a9ce9994dbe4c76cfa2aea
johnhaus <john.haus@outlook.com>
2019-09-13 20:37:48 -0500

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch, /* Only allows one route to be generated at a time */
+  } from "react-router-dom";
 import './App.css';
 import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   render() {
@@ -14,10 +19,13 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <div id="page-body">
-            <Route path="/" component={HomePage} exact />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesListPage} />
-            <Route path="/article/:name" component={ArticlePage} />
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} /> {/* Need to have this last */}
+            </Switch>
           </div>
         </div>
       </Router>
